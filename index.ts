0c425ac1e0b915b237dc2c90e4f5d05604b4bd8e
German Ceballos <germanceballos@tdf.edu.ar>
2022-09-10 23:18:52 -0300

    Trabajo 1 objeto auto

---
+++ index.ts
@@ -0,0 +1,58 @@
+class auto{
+
+    //Variables o atributos
+
+    public propietario:string;
+    public modelo:string;
+    public marca:string;
+    public color:string;
+    public ruedaAuxilio: boolean;
+
+    constructor(paramMarca:string, paramModelo:string, paramColor:string ){
+        this.marca = paramMarca;
+        this.modelo = paramModelo;
+        this.color = paramColor;
+        this.ruedaAuxilio = false;
+    }
+
+
+        
+    getPropietario():string{
+        return this.propietario;
+    }
+
+    setPropietario(paramPropietario:string):void{
+        this.propietario = paramPropietario;
+    }
+
+    getModelo():string{
+        return this.modelo;
+    }
+
+    setModelo(paramModelo: string):void{
+        this.modelo = paramModelo;
+    }
+
+    getColor():string{
+        return this.color;
+    }
+
+    setColor(paramColor:string):void{
+        this.color = paramColor;
+    }
+
+    getRuedaAuxilio():boolean{
+        return this.ruedaAuxilio;
+    }
+
+    setRuedaAuxilio():void{
+        this.ruedaAuxilio = !this.ruedaAuxilio;
+    }
+
+}
+
+let miAuto = new auto("VW","GOL POWER","BLANCO");
+
+let colorAuto : string = miAuto.getColor();
+
+console.log("Color del Auto: "+ colorAuto);
